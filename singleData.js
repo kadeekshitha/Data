e7a36b15db26a9adf2e69fd6641d930eaf27d5ba
Deekshitha k a <112818963+kadeekshitha@users.noreply.github.com>
2023-01-16 21:58:07 +0530

    first commit

---
+++ singleData.js
@@ -0,0 +1,338 @@
+{
+    "category":
+    [
+        {
+            "id": 1,
+            "category": "Lips"
+        },
+    
+        {
+            "id": 2,
+            "category": "Face"
+        },
+    
+        {
+            "id": 3,
+            "category": "Eyes"
+        },
+    
+        {
+            "id": 4,
+            "category": "Skin"
+        },
+    
+        {
+            "id": 5,
+            "category": "Gifts"
+        }
+    ],
+
+    "products": 
+        [
+            
+            {
+            "id":1 ,
+            "product-name":"Matte As Hell Crayon Lipstick",
+            "category":"Lips",
+            "category-id":1,
+            "price":849,
+            "Net Weight": 2.8,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/01_7db669dd-9541-4d04-ad5b-49653474339d.jpg?v=1640792033",
+            "color":[{
+                "color-id":1,
+                "color_name":"Red"
+            },
+            {
+                "color-id":2,
+                "color_name":"Fuchsia"   
+            }
+            ]
+            },
+        
+            {
+            "id":2 ,
+            "product-name":"Matte Attack Transferproof Lipstick ",
+            "category":"Lips",
+            "category-id":1,
+            "price":699,
+            "Net Weight": 2,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-matte-attack-transferproof-lipstick-06-spring-crimson-crimson-red-13678383726675_2e2c7596-c90f-4d0b-b121-dfb6df6fc0d4.jpg?v=1640760089",
+            "color":[{
+                "color-id":1,
+                "color_name":"Cardinal Pink"
+            },
+            {
+                "color-id":2,
+                "color_name":"Chilli Red"   
+            }
+            ]
+            },
+        
+            {
+            "id":3 ,
+            "product-name":"Nothing Else Matter Longwear Lipstick",
+            "category":"Lips",
+            "category-id":1,
+            "price":599,
+            "Net Weight": 3.5,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/1_5a928a42-8502-4b9b-9296-96d7423179de.jpg?v=1641961059",
+            "color":[{
+                "color-id":1,
+                "color_name":"Caramel Nude"
+            },
+            {
+                "color-id":2,
+                "color_name":"Red with hints of pink orange"   
+            }
+            ]
+            },
+        
+            {
+            "id":4 ,
+            "product-name":"Wonder Woman Creamy Matte Lipsticks",
+            "category":"Lips",
+            "category-id":1,
+            "price":299,
+            "Net Weight": 2,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-wonder-woman-creamy-matte-lipstick-05-love-child-toned-red-28122637566035.jpg?v=1619154921",
+            "color":"Toned Red"
+            },
+        
+            {
+            "id":5 ,
+            "product-name":"Mettle Liquid Lipstick",
+            "category":"Lips",
+            "category-id":1,
+            "price":1199,
+            "Net Volume": 7,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-mettle-liquid-lipstick-02-vega-deep-fuchsia-12067419553875.jpg?v=1619109886",
+            "color":"Deep Fuchsia"
+            },
+        
+            
+            {
+            "id":1 ,
+            "product-name":"SUGAR Prime Sublime Primer",
+            "category":"Face",
+            "category-id":2,
+            "price":499,
+            "Net weight": 15,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/BrighteningPrimer.jpg?v=1666886739",
+            "color":"Brightening"
+            },
+        
+            {
+            "id":2 ,
+            "product-name":"Dream Cover SPF15 Mattifying Compact",
+            "category":"Face",
+            "category-id":2,
+            "price":499,
+            "Net Weight": 6,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-dream-cover-spf15-mattifying-compact-10-latte-light-12775602454611.jpg?v=1619106458",
+            "color":"Latte (light)"
+            },
+        
+            {
+            "id":3 ,
+            "product-name":"Ace Of Face Foundation Stick",
+            "category":"Face",
+            "category-id":2,
+            "price":799,
+            "Net Weight": 7,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/17-Raf-1.jpg?v=1658419939",
+            "color":"Medium Beige Golden Peach Undertone"
+            },
+        
+            {
+            "id": 4 ,
+            "product-name":"Magic Wand Waterproof Concealer",
+            "category":"Face",
+            "category-id":2,
+            "price":849,
+            "Net Volume": 8.4,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-magic-wand-waterproof-concealer-07-vanilla-latte-fair-golden-undertone-12958386585683.jpg?v=1619114726",
+            "color":"Fair, Golden Undertone"
+            },
+        
+            {
+            "id":5 ,
+            "product-name":"Contour De Force Mini Blush",
+            "category":"Face",
+            "category-id":2,
+            "price":349,
+            "Net Volume": 4,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-contour-de-force-mini-blush-01-peach-peak-soft-peach-pink-12785043177555.jpg?v=1619102639",
+            "color":"Soft Peach Pink"
+            },
+            
+            
+            {
+            "id":1 ,
+            "product-name":"Eye Lit You So! Vivid Eyeliner ",
+            "category":"Eyes",
+            "category-id":3,
+            "price":699,
+            "Net Volume": 1.7,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-eye-lit-you-so-vivid-eyeliner-05-green-ray-27938831269971.jpg?v=1619153479",
+            "color":"Mint Green"
+            },
+        
+            {
+            "id":2 ,
+            "product-name":"Kohl Of Honour Intense Kajal",
+            "category":"Eyes",
+            "category-id":3,
+            "price":249,
+            "Net Volume": 1.7,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/01_5e8187e3-f7a2-40aa-a759-d4bf69769234.jpg?v=1639496327",
+            "color":"Black"
+            },
+        
+            {
+            "id":3 ,
+            "product-name":"Arch Arrival Brow Definer",
+            "category":"Eyes",
+            "category-id":3,
+            "price":499,
+            "Net Volume": 0.35,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/1_dfe1216c-0850-458c-8838-0abd46fd57e1.jpg?v=1640676576",
+            "color":"Medium Brown"
+            },
+        
+            {
+            "id":4,
+            "product-name":"Blend The Rules Eyeshadow Palette",
+            "category":"Eyes",
+            "category-id":3,
+            "price":1199,
+            "Net Volume": 10.4,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/01_0fa12791-2d87-4386-9633-fe0dea75cf8f.jpg?v=1639755487",
+            "color":"Warm Neutrals"
+            },
+        
+            {
+            "id":5 ,
+            "product-name":"Uptown Curl Lengthening Mascara",
+            "category":"Eyes",
+            "category-id":3,
+            "price":499,
+            "Net Volume": 5,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-uptown-curl-lengthening-mascara-01-black-beauty-black-28122637828179.jpg?v=1619106532",
+            "color":"Black"
+            },
+            
+            
+            {
+            "id":1 ,
+            "product-name":"Bling Leader Illuminating Moisturizer",
+            "category":"Skin",
+            "category-id":4,
+            "price":699,
+            "Net Volume": 25 ,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-bling-leader-illuminating-moisturizer-01-gold-diggin-warm-gold-with-a-pearl-finish-13644788006995.jpg?v=1619115785",
+            "color":"01 Gold Diggin"
+            },
+        
+            {
+            "id":2 ,
+            "product-name":"Bling Leader Illuminating Sunscreen SPF35 PA+++",
+            "category":"Skin",
+            "category-id":4,
+            "price":599,
+            "Net Volume": 50 ,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/1_e5df3bb8-4439-4980-ada2-aa90b126ada3.jpg?v=1651852746",
+            "color":"02 Pink Trippin"
+            },
+            
+            {
+            "id":3 ,
+            "product-name":"Grand Finale Dewy Setting Mist",
+            "category":"Skin",
+            "category-id":4,
+            "price":699,
+            "Net Volume": 50 ,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-grand-finale-dewy-setting-mist-14890489184339.jpg?v=1619121429",
+            "color":null
+            },
+        
+            {
+            "id":4 ,
+            "product-name":"Charcoal Patrol Bubble Mask ",
+            "category":"Skin",
+            "category-id":4,
+            "price":699,
+            "Net Weight": 20,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-charcoal-patrol-bubble-mask-pack-of-6-12784621224019.jpg?v=1619114547",
+            "color":null
+            },
+        
+            {
+            "id":5 ,
+            "product-name":"Coffee Culture Brightening Serum",
+            "category":"Skin",
+            "category-id":4,
+            "price":499,
+            "Net Volume": 15,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/CCBrighteningSerumWBG-3.jpg?v=1628610327",
+            "color":null
+            },
+            
+            
+            {
+            "id":1 ,
+            "product-name":"Vineeta's Favourite Makeup Kit",
+            "category":"Gifts",
+            "category-id":5,
+            "price":2999,
+            "size":null,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/Vineeta_s-Favourite-Makeup-kit---WBG-images.jpg?v=1669626013",
+            "color":null
+            },
+        
+            {
+            "id":2 ,
+            "product-name":"Anniversary Kit",
+            "category":"Gifts",
+            "category-id":5,
+            "price":1999,
+            "size":null,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/1_0ca61ef7-05ce-49be-9fa1-de7c41b99a1f.jpg?v=1668050175",
+            "color":null
+            },
+        
+            {
+            "id":3 ,
+            "product-name":"Hi-5 Kit",
+            "category":"Gifts",
+            "category-id":5,
+            "price":999,
+            "size":null,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/Hi-5-KIT-WBG-Images1-SEP22.jpg?v=1663689606",
+            "color":null
+            },
+        
+            {
+            "id":4 ,
+            "product-name":"Minimal Beauty Set",
+            "category":"Gifts",
+            "category-id":5,
+            "price":1249,
+            "size":null,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/Minimal-beauty-set-wbg-01.jpg?v=1635349523",
+            "color":null
+            },
+        
+            {
+            "id":5 ,
+            "product-name":"Sweat No More Makeup Kit",
+            "category":"Gifts",
+            "category-id":5,
+            "price":2699,
+            "size":null,
+            "image":"https://cdn.shopify.com/s/files/1/0906/2558/products/Launch---Sweat-No-More-Makeup-Kit---WBG-Images_11.jpg?v=1653494527",
+            "color":null
+            } 
+        ]
+    
+    
+}
